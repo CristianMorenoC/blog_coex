6ab3752ad0fdb282e336e37d660656f25e5ecdc9
JP <cala.js@icloud.com>
2022-09-06 16:51:28 -0500

    before checkout

--- src/controllers/comment.controller.ts
+++ src/controllers/comment.controller.ts
@@ -3,5 +3,14 @@ import { IStatus } from '../util/status.interface'
 
 export interface ICommentController {
     createComment(req: Request, res: Response): Promise<IStatus>
-    createComment(req: Request, res: Response): Promise<IStatus>
+    deleteComment(req: Request, res: Response): Promise<IStatus>
+}
+
+export class CommentController implements ICommentController {
+    constructor() {}
+    public async createComment(req: Request, res: Response): Promise<IStatus> {
+        try {
+            let data = req.body
+        } catch (err) {}
+    }
 }
