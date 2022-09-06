9673e4d9d2ab483f294732a93f5bae0639910d11
Julian Garcia <jcgrrincon@gmail.com>
2022-09-06 16:29:30 -0500

    integracion de morgan como middleware

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express, { Express } from "express";
+import morgan from 'morgan';
 
 class ServerConfig{
     private _app:Express = express();
 
     constructor() {
         this._app.set('PORT', 4000);
+        this._app.use(express.json());
+        this._app.use(morgan('dev'));
     }
 
     get app(){
