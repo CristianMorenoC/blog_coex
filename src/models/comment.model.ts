3a40542040867a5e750e1ada92072785c1ba4180
JP <cala.js@icloud.com>
2022-09-06 17:20:35 -0500

    before merge, Comment Controller almost finished

--- src/models/comment.model.ts
+++ src/models/comment.model.ts
@@ -1,24 +1,28 @@
-import { IStatus } from "../util/status.interface";
+import { IStatus } from '../util/status.interface'
 
 export interface ICommentModel {
-    createComment(data: {post_id: number, user_id: number, content: string}):Promise<IStatus>,
-    getCommentsFromPost(data: {post_id: number}):Promise<any[]>,
-    deleteComment(data: {comment_id: number}):Promise<IStatus>
+    createComment(data: {
+        post_id: number
+        user_id: number
+        content: string
+    }): Promise<IStatus>
+    getCommentsFromPost(data: { post_id: number }): Promise<any[]>
+    deleteComment(data: { comment_id: number }): Promise<IStatus>
 }
 
 class CommentModel implements ICommentModel {
-    constructor(){
-
+    constructor() {}
+    createComment(data: {
+        post_id: number
+        user_id: number
+        content: string
+    }): Promise<IStatus> {
+        throw new Error('Method not implemented.')
     }
-    createComment(data: { post_id: number; user_id: number; content: string; }): Promise<IStatus> {
-        throw new Error("Method not implemented.");
+    getCommentsFromPost(data: { post_id: number }): Promise<any[]> {
+        throw new Error('Method not implemented.')
     }
-    getCommentsFromPost(data: { post_id: number; }): Promise<any[]> {
-        throw new Error("Method not implemented.");
-    }
-    deleteComment(data: { comment_id: number; }): Promise<IStatus> {
-        throw new Error("Method not implemented.");
+    deleteComment(data: { comment_id: number }): Promise<IStatus> {
+        throw new Error('Method not implemented.')
     }
 }
-
-
