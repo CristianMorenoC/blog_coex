542aa37eae4086998ad3e4503a15db07559347b0
Julian Garcia <jcgrrincon@gmail.com>
2022-09-06 17:20:07 -0500

    modelo de comentario casi lista, solo falta el delete

--- src/models/comment.model.ts
+++ src/models/comment.model.ts
@@ -1,24 +1,40 @@
 import { IStatus } from "../util/status.interface";
+import { addDoc, collection, getDoc, setDoc, doc  } from "firebase/firestore";
+import { DBConfig, db } from "../util/firebase";
 
 export interface ICommentModel {
-    createComment(data: {post_id: number, user_id: number, content: string}):Promise<IStatus>,
-    getCommentsFromPost(data: {post_id: number}):Promise<any[]>,
-    deleteComment(data: {comment_id: number}):Promise<IStatus>
+    createComment(data: {post_id: string, user_id: string, content: string}):Promise<IStatus>,
+    getCommentsFromPost(data: {post_id: string}):Promise<IStatus>,
+    deleteComment(data: {comment_id: string}):Promise<IStatus>
 }
 
 class CommentModel implements ICommentModel {
-    constructor(){
+    constructor(
+        private db:DBConfig
+    ){
+    }
+    async createComment(data: { post_id: string; user_id: string; content: string; }): Promise<IStatus> {
+        try {
+            const ref = await addDoc(collection(this.db.dbConnection, "comments"), data);
 
+            return {status: true, info: `comentario creado con el id: ${ref.id}`}
+        } catch (err) {
+            console.log(err)
+            return {status: false, info: `el comentario no pudo ser creado ${err}`}
+        }
     }
-    createComment(data: { post_id: number; user_id: number; content: string; }): Promise<IStatus> {
-        throw new Error("Method not implemented.");
+    async getCommentsFromPost(data: { post_id: string; }): Promise<IStatus> {
+        try {
+            const comment = await getDoc(doc(this.db.dbConnection, "comments", data.post_id));
+            return {status: true, info: `${comment}`}
+        } catch (err) {
+            console.log(err)
+            return {status: false, info: `el comentario no pudo ser creado ${err}`}
+        }
     }
-    getCommentsFromPost(data: { post_id: number; }): Promise<any[]> {
-        throw new Error("Method not implemented.");
-    }
-    deleteComment(data: { comment_id: number; }): Promise<IStatus> {
+    deleteComment(data: { comment_id: string; }): Promise<IStatus> {
         throw new Error("Method not implemented.");
     }
 }
 
-
+export default new CommentModel(db)
